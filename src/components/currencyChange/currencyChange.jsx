4cc2cb1960edd0d155f830761ea8e573668bcd26
Júlia Acosta <juliagabriela98@gmail.com>
2023-03-03 22:43:48 -0300

    add function to change input and calculate the value

--- src/components/currencyChange/currencyChange.jsx
+++ src/components/currencyChange/currencyChange.jsx
@@ -2,62 +2,76 @@ import styles from "./styles.module.css";
 import changeIcon from "../../assets/changeIcon.svg";
 
 export default function CurrencyChange({
-  currency,
-  changeCurrency,
-  currencyOptions,
+    currency,
+    changeCurrency,
+    currencyOptions,
 }) {
-  return (
-    <div>
-      <h2>Conversor de moedas</h2>
-      <div className={styles.slotsSwitch}>
-        <div tabIndex={0} className={styles.currency}>
-          <input
-            value={currency.first.value}
-            className={styles.value}
-            placeholder="R$"
-            onChange={(e) => changeCurrency("first", "value", e.target.value)}
-          ></input>
-          <div className={styles.divider}></div>
-          <select
-            className={styles.exchange}
-            defaultValue={currency.first.currency}
-            id="first-currency-selector"
-            onChange={(e) =>
-              changeCurrency("first", "currency", e.target.value)
-            }
-          >
-            {currencyOptions.map((actualCurrency) => (
-              <option>{actualCurrency.currentCurrency}</option>
-            ))}
-          </select>
-        </div>
-        <img src={changeIcon} alt="icon with two arrows oposites" />
+    return (
         <div>
-          <div className={styles.currency}>
-            <input
-              value={currency.second.value}
-              className={styles.value}
-              placeholder="R$"
-              onChange={(e) =>
-                changeCurrency("second", "value", e.target.value)
-              }
-            />
-            <div className={styles.divider}></div>
-            <select
-              className={styles.exchange}
-              defaultValue={currency.second.currency}
-              onChange={(e) =>
-                changeCurrency("second", "currency", e.target.value)
-              }
-              id="second-currency-selector"
-            >
-              {currencyOptions.map((actualCurrency) => (
-                <option>{actualCurrency.currentCurrency}</option>
-              ))}
-            </select>
-          </div>
+            <h2>Conversor de moedas</h2>
+            <div className={styles.slotsSwitch}>
+                <div tabIndex={0} className={styles.currency}>
+                    <input
+                        value={currency.first.value}
+                        className={styles.value}
+                        placeholder="R$"
+                        onChange={(e) =>
+                            changeCurrency("first", "value", e.target.value)
+                        }
+                    ></input>
+                    <div className={styles.divider}></div>
+                    <select
+                        className={styles.exchange}
+                        defaultValue={currency.first.currency}
+                        id="first-currency-selector"
+                        onChange={(e) =>
+                            changeCurrency("first", "currency", e.target.value)
+                        }
+                    >
+                        {currencyOptions.map((actualCurrency) => (
+                            <option key={actualCurrency.currentCurrency}>
+                                {actualCurrency.currentCurrency}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <img src={changeIcon} alt="icon with two arrows oposites" />
+                <div>
+                    <div className={styles.currency}>
+                        <input
+                            value={currency.second.value}
+                            className={styles.value}
+                            placeholder="R$"
+                            onChange={(e) =>
+                                changeCurrency(
+                                    "second",
+                                    "value",
+                                    e.target.value
+                                )
+                            }
+                        />
+                        <div className={styles.divider}></div>
+                        <select
+                            className={styles.exchange}
+                            defaultValue={currency.second.currency}
+                            onChange={(e) =>
+                                changeCurrency(
+                                    "second",
+                                    "currency",
+                                    e.target.value
+                                )
+                            }
+                            id="second-currency-selector"
+                        >
+                            {currencyOptions.map((actualCurrency) => (
+                                <option key={actualCurrency.currentCurrency}>
+                                    {actualCurrency.currentCurrency}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,92 +1,99 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "./styles.module.css";
 
 import CurrencyChange from "../components/currencyChange/currencyChange";
-import ChartExchange from "../components/chartExchange/chartExchange";
 import HistoricChart from "../components/historicChart/historicChart";
 
 const currencyOptions = [
-  {
-    currentCurrency: "BRL",
-    rate: {
-      USD: 5.1932,
-      EUR: 5.5144,
-      GBP: 6.24524,
-      CHF: 0.180283,
+    {
+        currentCurrency: "BRL",
+        rate: {
+            USD: 5.1932,
+            EUR: 5.5144,
+            GBP: 6.24524,
+            CHF: 0.180283,
+        },
     },
-  },
-  {
-    currentCurrency: "USD",
-    rate: {
-      BRL: 2,
-      EUR: 3,
-      GBP: 5,
-      CHF: 1.064541,
+    {
+        currentCurrency: "USD",
+        rate: {
+            BRL: 2,
+            EUR: 3,
+            GBP: 5,
+            CHF: 1.064541,
+        },
     },
-  },
-  {
-    currentCurrency: "EUR",
-    rate: {
-      USD: 2,
-      BRL: 3,
-      GBP: 5,
-      CHF: 3,
+    {
+        currentCurrency: "EUR",
+        rate: {
+            USD: 2,
+            BRL: 3,
+            GBP: 5,
+            CHF: 3,
+        },
     },
-  },
-  {
-    currentCurrency: "GBP",
-    rate: {
-      USD: 2,
-      EUR: 3,
-      BRL: 5,
-      CHF: 3,
+    {
+        currentCurrency: "GBP",
+        rate: {
+            USD: 2,
+            EUR: 3,
+            BRL: 5,
+            CHF: 3,
+        },
     },
-  },
-  {
-    currentCurrency: "CHF",
-    rate: {
-      USD: 2,
-      EUR: 3,
-      GBP: 5,
-      BRL: 3,
+    {
+        currentCurrency: "CHF",
+        rate: {
+            USD: 2,
+            EUR: 3,
+            GBP: 5,
+            BRL: 3,
+        },
     },
-  },
 ];
 
 function HomePage() {
-  const [currency, setCurrency] = useState({
-    first: { value: 1000, currency: "BRL" },
-    second: { value: 5000, currency: "USD" },
-  });
+    const [currency, setCurrency] = useState({
+        first: { value: 1000, currency: "BRL" },
+        second: { value: 5193.2, currency: "USD" },
+    });
 
-  const changeCurrency = (slot, value) => {
-    setCurrency((prev) => ({
-      ...prev,
-      [slot]: { ...prev[slot], currency: parseFloat(value) },
-    }));
-  };
+    const changeCurrency = (slot, subslot, value) => {
+        const tempCurrency = {
+            ...currency,
+            [slot]: { ...currency[slot], [subslot]: value },
+        };
 
-  const changeValue = (slot, value) => {
-    if (slot === "first")
-      setCurrency((prev) => ({
-        first: { value: value, currency: "BRL" },
-        second: { value: 5000, currency: "USD" },
-      }));
-  };
+        const index = currencyOptions.findIndex(
+            (currency) =>
+                currency.currentCurrency === tempCurrency[slot].currency
+        );
 
-  return (
-    <div className={styles.container}>
-      <div>
-        <CurrencyChange
-          currency={currency}
-          changeCurrency={changeCurrency}
-          currencyOptions={currencyOptions}
-        />
-        <ChartExchange />
-        <HistoricChart />
-      </div>
-    </div>
-  );
+        if (slot === "first") {
+            tempCurrency.second.value =
+                parseFloat(tempCurrency.first.value) *
+                currencyOptions[index].rate[tempCurrency.second.currency];
+        } else {
+            tempCurrency.first.value =
+                parseFloat(tempCurrency.second.value) *
+                currencyOptions[index].rate[tempCurrency.first.currency];
+        }
+
+        setCurrency(tempCurrency);
+    };
+
+    return (
+        <div className={styles.container}>
+            <div>
+                <CurrencyChange
+                    currency={currency}
+                    changeCurrency={changeCurrency}
+                    currencyOptions={currencyOptions}
+                />
+                <HistoricChart />
+            </div>
+        </div>
+    );
 }
 
 export default HomePage;
